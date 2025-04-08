1cb65b18fd823f3dcfae1c899da282547cf140f5
1140074 <alejandro.guerrah@elektra.com.mx>
2025-04-08 16:58:35 -0600

    Initial Commit

---
+++ ProjectLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectLayout
+//
+//  Created by Alejandro Guerra Hernandez on 08/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
